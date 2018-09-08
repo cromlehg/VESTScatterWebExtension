fee73b43c9cef27b709480d5dc280627f8740bc7
Arkadiy A.Davydov <arkadiy.a.davydov@gmail.com>
2018-09-08 23:12:53 +0300

    chore(plugins): replace EOS Mainnet with VEST Mainnet

--- src/plugins/defaults/eos.js
+++ src/plugins/defaults/eos.js
@@ -32,11 +32,11 @@ export default class EOS extends Plugin {
     async getEndorsedNetwork(){
         return new Promise((resolve, reject) => {
             resolve(new Network(
-                'EOS Mainnet', 'https',
-                'nodes.get-scatter.com',
-                443,
-                Blockchains.EOS,
-                'aca376f206b8fc25a6ed44dbdc66547c36c6c33e3a119ffbeaef943642f0e906'
+                'VEST Mainnet', 'http',
+                '148.251.194.72',
+                8888,
+                Blockchains.VEST,
+                '8449f44ce9ba91e7b3da01900a6c6e49cd8466ceb43352f0902f6934dbfc9f4a'
             ));
         });
     }
